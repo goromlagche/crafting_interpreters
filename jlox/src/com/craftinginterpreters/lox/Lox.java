8301a4f06f7b67f1e8ddfa332ed5976957f48593
goromlagche <4189070+goromlagche@users.noreply.github.com>
2020-01-01 00:58:50 +0530

    code for ch6

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -43,13 +43,22 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expr = parser.parse();
 
-        for(Token token : tokens){
-            System.out.println(token);
+        if(hadError) return;
+        System.out.println(new AstPrinter().print(expr));
+    }
+
+    static void error(Token token, String message) {
+        if (token.tokenType == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
 
-    static void error(int line, String message){
+    static void error(int line, String message) {
         report(line, "", message);
     }
 
@@ -57,6 +66,5 @@ public class Lox {
         System.err.println("[Line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
-
 
 }
